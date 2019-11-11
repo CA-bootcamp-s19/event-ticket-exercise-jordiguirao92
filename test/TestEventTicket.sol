44d5fd3dbd61f09a6155872882b03d5d9d02d8b5
jordiguirao92 <jordiguirao92@users.noreply.github.com>
2019-11-11 19:25:25 +0100

    Update

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -1,31 +1,25 @@
 pragma solidity ^0.5.0;
 
-//import "truffle/Assert.sol";
-//import "truffle/DeployedAddresses.sol";
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
 import "../contracts/EventTickets.sol";
 import "../contracts/EventTicketsV2.sol";
 
 contract TestEventTicket {
 
-string description = "description";
-string website = "URL"; 
-uint ticketNumbers = 100; 
+function () external payable {}
 
 //Tests of the contract EventTickets.sol
-function testInitialConstructor() public {
- EventTickets eventTicket = new EventTickets(description, website, ticketNumbers);
-
- assert.equal(eventTicket.myEvent.description, description);
- assert.equal(eventTicket.myEvent.website, website);
- assert.equal(eventTicket.myEvent.totalTickets, ticketNumbers);
+function testInitialConstructorIsOpen() public {
+ EventTickets eventTicket = new EventTickets("new web", "URL", 100);
+ (,,,,bool isOpen) = eventTicket.readEvent();
+ Assert.isTrue(isOpen, "when the contract is created sale is open");
 }
 
-
-
-//Test of the contract EventTicketsV2.sol
-
-
-
-
+function testInitialConstructorWebsite() public {
+ EventTickets eventTicket = new EventTickets("new web", "URL", 100);
+ (,string memory website,,,) = eventTicket.readEvent();
+ Assert.equal(website, "URL", "The website has to be the same");
+}
 
 }
